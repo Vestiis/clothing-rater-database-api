8e0b48acdbcd7b8e5da92c80f139de39b9fcb59a
Guillaume LIGNER <guillaume.ligner@gmail.com>
2021-11-10 18:13:55 +0100

    (hot update) Use a new db connection

--- database/config.py
+++ database/config.py
@@ -9,7 +9,7 @@ class Config:
     POSTGRES_SECRETS = json.loads(
         secrets.access_secret_version(
             request={
-                "name": "projects/439044485118/secrets/postgres_secrets/versions/3"
+                "name": "projects/439044485118/secrets/postgres_secrets/versions/4"
             }
         ).payload.data.decode("utf-8")
     )
@@ -18,5 +18,5 @@ class Config:
         f"postgresql+psycopg2://{POSTGRES_SECRETS['user']}:"
         f"{POSTGRES_SECRETS['password']}"
     )
-    INSTANCE_CONNECTION = "impactful-ring-314819:europe-west1:clothing-rater-postgres"
+    INSTANCE_CONNECTION = "impactful-ring-314819:europe-west1:clothing-rater-postgresql"
     SQLALCHEMY_DATABASE_URI = f"{SQLALCHEMY_DATABASE_BASE}@/{POSTGRES_DB}?host=/cloudsql/{INSTANCE_CONNECTION}"
